d3eb2d367619d90b1be610062ef23a76175e3c95
Parth Chavhan <parthjain9460@gmail.com>
2025-02-03 13:32:25 +0530

    mssg size

--- chunk_processing.py
+++ chunk_processing.py
@@ -2,7 +2,7 @@ from gemini_processing import process_text_with_gemini
 from file_processing import process_file
 from file_processing import clean_json
 
-def process_in_chunks(txt_data, chunk_size=20):
+def process_in_chunks(txt_data, chunk_size=40):
     # Initialize results
     results = []
     txt_data = process_file(txt_data)  # Clean the raw text data
